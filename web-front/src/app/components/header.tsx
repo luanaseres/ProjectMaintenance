72134dcdcf34d26c94e67b25079c49f3b8ba57be
Luana Seres Girotto <123522626+LuanaSeres@users.noreply.github.com>
2024-08-02 19:42:32 -0300

    feat: iniciando o projeto e a tela home
    iniciei o projeto e instalei as dependencias, em seguida comecei um esboço da tela home

---
+++ web-front/src/app/components/header.tsx
@@ -0,0 +1,15 @@
+
+export function Header(){
+  
+  return(
+    <header className="h-12 p-3 bg-customBlue text-white flex justify-between">
+        <h1>Manutenção</h1>
+        <div className="flex gap-4">
+          <p>Home</p>
+          <p>Sobre</p>
+          <p>Contato</p>
+        </div>
+        
+      </header>
+  );
+} 
---
+++ web-front/src/app/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Header } from "./components/header";
+import { Footer } from "./components/footer";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="">
+        <Header/>
+        <main>
+          <div className="bg-customBlue h-96">
+
+          </div>
+          <div className="bg-customGrey h-96">
+
+          </div>
+
+        </main>
+      </div>
+
+     <Footer/>
+
+    </div>
+  );
+}
