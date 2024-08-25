64771fbf1e90a8e4d96a86915a03b5179b61ca38
Luana Seres Girotto <luana.sgirotto@gmail.com>
2024-08-25 18:45:37 -0300

    feat: criado pagina para o controle de custos

---
+++ web-front/src/app/controleCustos/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Header } from "../components/header";
+import { Footer } from "../components/footer";
+import { ControleCustos } from "../components/controle-custos";
+
+
+export default function ControleC() {
+  return(
+    <div className="flex flex-col h-screen">
+      <Header/>
+
+      <main className="flex-grow overflow-y-auto bg-gray-100 p-4">
+        <ControleCustos/>
+      </main>
+
+      <Footer/>
+    </div>
+  );
+}
