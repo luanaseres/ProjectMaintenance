2564696a20891823e4f52df544875fcb895e3325
Luana Seres Girotto <luana.sgirotto@gmail.com>
2024-08-25 18:37:13 -0300

    feat: pagina home terminada

--- web-front/src/app/page.tsx
+++ web-front/src/app/page.tsx
@@ -5,39 +5,36 @@ import { Tabela } from "./components/tabela";
 import { Card } from "./components/card";
 
 import { HiOutlineWrenchScrewdriver } from "react-icons/hi2";
-import { AiOutlineEnvironment } from "react-icons/ai";
+import { LuForklift } from "react-icons/lu";
 import { CgNotes } from "react-icons/cg";
 import { FaRegCircleCheck } from "react-icons/fa6";
 
 export default function Home() {
 
   const cards = [
-    {color: "bg-orange-200", qty: "200", text: "Máquinas", icon: <AiOutlineEnvironment size={48} />},
-    {color: "bg-blue-200", qty: "200", text: "Equipamentos", icon: <HiOutlineWrenchScrewdriver size={48} />},
-    {color: "bg-red-200", qty: "200", text: "Máquinas", icon: <CgNotes size={48} />},
-    {color: "bg-green-200", qty: "200", text: "O.S. Positivas", icon: <FaRegCircleCheck size={48} />},
+    {color: "bg-orange-200", qty: "100", text: "Máquinas", icon: <LuForklift size={48} />},
+    {color: "bg-blue-200", qty: "350", text: "Equipamentos", icon: <HiOutlineWrenchScrewdriver size={48} />},
+    {color: "bg-red-200", qty: "30", text: "O.S. Negativas", icon: <CgNotes size={48} />},
+    {color: "bg-green-200", qty: "50", text: "O.S. Positivas", icon: <FaRegCircleCheck size={48} />},
   ]
   
   return (
     <div className="h-screen flex flex-col bg-customBlue">
-      <div className="">
-        <Header/>
-        <main>
-          <div className="h-full mt-3">
-            <div className="grid grid-cols-4 gap-4 p-6">
-
-              {cards.map((props) => (
-                <Card color={props.color} qty={props.qty} text={props.text} icon={props.icon}/>
-              ))}
-
-            </div>
-          </div>
-          <div className="bg-customGrey h-full">
-            <Tabela/>
-          </div>
-        
-        </main>
+      <Header/>
+      <div className="mt-3">
+        <div className="grid grid-cols-4 gap-4 p-6 bg-customBlue">
+          {cards.map((props) => (
+            <Card color={props.color} qty={props.qty} text={props.text} icon={props.icon}/>
+          ))}
+        </div>
       </div>
+
+      <main className="flex-grow overflow-y-auto bg-gray-100 p-4">
+        <div className="bg-customGrey h-full">
+          <Tabela/>
+        </div>
+      </main>
+
 
      <Footer/>
 
