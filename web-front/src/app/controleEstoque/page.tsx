df67aa04f65f91be463f3daf5d0089a8ead95537
Luana Seres Girotto <luana.sgirotto@gmail.com>
2024-08-25 18:45:57 -0300

    feat: criado pagina para o controle de estoque

---
+++ web-front/src/app/controleEstoque/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Header } from "../components/header";
+import { Footer } from "../components/footer";
+import { ControleEstoque } from "../components/controle-estoque";
+
+
+export default function ControleE() {
+  return(
+    <div className="flex flex-col h-screen">
+      <Header/>
+
+      <main className="flex-grow overflow-y-auto bg-gray-100 p-4">
+        <ControleEstoque/>
+      </main>
+
+      <Footer/>
+    </div>
+  );
+}
