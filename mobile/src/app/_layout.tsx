eca0e2d6cf148c0b9647ce938708ec5b03b8d7e7
Luana Seres Girotto <123522626+LuanaSeres@users.noreply.github.com>
2024-09-11 14:20:42 -0300

    feat: aulas de mobile

---
+++ mobile/src/app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+import Index from ".";
+
+export default function RootLayout() {
+  return (
+    <Index/>
+  );
+}
---
+++ mobile/src/app/home.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function Home(){
+  return(
+    <Text>Página Home</Text>
+  )
+}
