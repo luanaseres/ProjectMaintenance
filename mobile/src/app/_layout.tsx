08574e8c5b9c427d7fe505fc1b466fe5de20717a
TaliaGoncalves <gtalia246@gmail.com>
2024-09-12 21:57:55 -0300

    feat: configurando layout que sinaliza as telas criadas

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,7 +1,10 @@
-import Index from ".";
+import { Stack } from 'expo-router/stack';
 
-export default function RootLayout() {
+export default function Layout() {
   return (
-    <Index/>
+    <Stack initialRouteName='login'>
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
   );
 }
