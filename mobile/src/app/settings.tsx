3b21a05b212a73e665b230c6a609afe070601818
Luana Seres Girotto <123522626+LuanaSeres@users.noreply.github.com>
2024-09-16 14:15:03 -0300

    feat: criado um caminho para voltar a pagina anterior e alterado a pasta

--- mobile/src/app/settings.tsx
+++ mobile/src/app/settings.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'expo-router';
 import { useState } from 'react';
 import { View, Text, TextInput, Switch, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Settings() {
   const [user, setUser] = useState({
@@ -16,6 +17,13 @@ export default function Settings() {
 
   return (
     <SafeAreaView className="flex-1 bg-customBlue">
+      <View className='flex flex-row w-full align-middle text-center mt-4'>
+        <Link href="/(tabs)" className='mr-4'>
+          <AntDesign name="arrowleft" size={30} color="white" />
+        </Link>
+        <Text className="text-xl font-semibold text-white mb-2 text-center">Configurações</Text>
+      </View>
+      
       <ScrollView className="flex-1 p-4">
         {/* Perfil do Usuário */}
         <View className="mb-6">
@@ -55,7 +63,7 @@ export default function Settings() {
 
         {/* Logout */}
         <TouchableOpacity className="bg-red-600 p-3 rounded-md">
-          <Link href={"/login"}>
+          <Link href={"/"}>
             <Text className="text-white text-center text-lg">Sair</Text>
           </Link>
         </TouchableOpacity>
